761790fb4eb6337645b22c2ecdf12fe01b016d1d
Rodrigo Lacal Alejandre <rlacal@agenormantenimientos.com>
2026-01-07 16:32:58 +0100

    actualizacion django

---
+++ plantillas/plantillas/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def simple(request):
+    return render(request, 'simple.html', {})
+
+
+def dinamico(request, name):
+    categories = ['code', 'desing', 'marketing', 'bussiness']
+    context = {'name': name, 'categories': categories}
+    return render(request, 'dinamico.html', context)
+
+
